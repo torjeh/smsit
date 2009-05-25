0ded25fc70d2e912fb3e104632ee0d38ac4ffd65
Torje Starbo Henriksen <torjeh@nst1201.ad.telemed.no>
2009-05-25 21:13:51 +0200

    Committer: Torje Starbo Henriksen <torje.starbo.henriksen@ntelemed.no> On branch master Changes to be committed: 	modified:   smsit.py
    Added an alert method and some checks to figure out if hosts are down. Only supports IP-addresses now!

--- smsit.py
+++ smsit.py
@@ -3,12 +3,17 @@ Check for hosts that are down.
 Report by sending SMS
 
 
-We could make this application multithreaded,
-but there is no use at this point.
-
 *) This program needs to run as root!
-*) This program is python 3.x compatible
 *) 
+
+
+TODO: 
+T) Make python 3.x compatible
+T) Multithreaded? :)
+T) Deamon
+T) Config
+T) Service
+T) SMS-script
 
 
 Torje S. Henriksen
@@ -28,17 +33,17 @@ a configuration file (e.g. /etc/smsit.conf
 """
 
 # List of hosts that should be checked
-hosts = {"pbg4.local":0, "localhost":0, "somebody":0}
+hosts = {"10.0.1.4":0, "127.0.0.1":0, "10.0.1.23":0}
 
 # Phone number(s) (string or int, I don't care)
 phone_no = [90912307] # Torje
 
 # Number of checks a host must be down in a row to set off the alarm (send SMS)
-alarm_treshold = 3
+alert_treshold = 3
 # Number of minutes between each check
 check_time = 5  # minutes
 # Script to run to send the SMS (yeah, kind of diirty)
-alert_sh = "/home/torjeh/local/bin/send_sms.sh"
+alert_sh = "./send_sms.sh"
 
 
 """ Init: """ 
@@ -47,26 +52,46 @@ alert_sh = "/home/torjeh/local/bin/send_sms.sh"
 
 """ Body """
 
-# This method is stolen from 
+# This method is (pretty much) stolen from 
 # http://www.wellho.net/solutions/python-python-threads-a-first-example.html
-
 def test_ping_hosts(hosts):
     lifeline = re.compile(r"(\d) received")
     report = ("No response","Partial Response","Alive")
-    print "Testing " + str(len(hosts)) + " hosts."
+    #print "Testing " + str(len(hosts)) + " hosts."
     for h in hosts:
-        pingaling = os.popen("ping -q -c2 " + h, "r") # do the ping
+        pingaling = os.popen("ping -q -W 5 -c2 " + h, "r") # do the ping
         print h, 
-        sys.stdout.flush() #?
+        sys.stdout.flush() 
         while 1:
             line = pingaling.readline()
-            if not line: break
-            igot = re.findall(lifeline,line)
+            if not line: break 
+            igot = re.findall(lifeline,line) # Parse
             if igot:
                 print report[int(igot[0])]
-    print time.ctime()
+                if int(igot[0]) == 0: hosts[h] += 1
+                else: hosts[h] = 0
 
-test_ping_hosts(hosts)
 
-for h in hosts:
-    print h + " " + str(hosts[h])
+
+
+def host_down(hosts, alert_treshold):
+    rl = [] # return value - list of hosts that are down
+    for h in hosts:
+        if hosts[h] >= alert_treshold:
+            rl.append(h)
+    return rl
+        
+def alert(down):
+    print str(len(down)) + " hosts are down!" 
+
+# Forever
+while (1):
+    test_ping_hosts(hosts)
+    down = host_down(hosts, alert_treshold) 
+    if len(down) > 0: # hosts are down
+        alert(down)         
+
+
+
+    #for h in hosts:
+    #   print h + " " + str(hosts[h])
